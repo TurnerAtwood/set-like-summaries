dcf502132aac550c7ae4820711927b9c9446711e
TurnerAtwood <Turner.Atwood.0@gmail.com>
2020-04-30 14:23:45 -0500

    Added quick fix for dependency install

---
+++ dependency_install.py
@@ -0,0 +1,13 @@
+# Quick fix to install all of the dependencies for the package
+import os
+
+DEPENDENCIES = ['scipy', 'cython', 'numpy', 'sister', 'nltk', 'rouge', 'progressbar']
+
+def main():
+	for dep in DEPENDENCIES:
+		os.system(f'pip install {dep}')
+
+if __name__ == "__main__":
+	main()
+
+
